2ad49c53581f2b286c90390f575b23a9f9755fca
Kanav Arora <kanavarora2002@gmail.com>
2020-05-26 21:05:24 +0530

    Update Type3.java

--- Anagram/Type3.java
+++ Anagram/Type3.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 public class Type3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		In this approach we will set the number of repetition of a character in an array. For example for 
+		aab, a-2 and b-1. Now other string will be sorted characterwise using for each loop and the index
+		will be decreased by one. After last execution if the value of all chars of first array is 0, they
+		are anagrams.
+		*/
 		Scanner sc = new Scanner(System.in);
 		System.out.println("First word");
 		String a = sc.nextLine();
